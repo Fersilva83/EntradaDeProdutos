e174694cd6ce7bce65510317d45c21cda990b19b
Fernanda Rodrigues <93264511+Fersilva83@users.noreply.github.com>
2022-01-22 18:48:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine(" Entre os dados do produto : ");
+            Console.Write("Nome:");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preco:");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidad:");
+            p.Quantidade= int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(" Dados do produto " + p);
+
+        }
+    }
+}
